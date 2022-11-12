5caae6ea7c309f894d3ff1a532a9588b4aac86b7
Simon <simon@benezan.de>
2022-11-12 10:22:25 +0100

    variable database path

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,8 +1,11 @@
 import { Config, JsonDB } from "node-json-db";
+import { env } from "../environment.js";
 import type DatabaseAdapterInterface from "./database-adapter.js";
 import JsonDatabaseImpl from "./json-database.js";
 
-const defaultImpl: DatabaseAdapterInterface = new JsonDatabaseImpl(new JsonDB(new Config("security/database.json", true, true, "/", true)));
+const defaultImpl: DatabaseAdapterInterface = new JsonDatabaseImpl(
+  new JsonDB(new Config(env("DATABASE_PATH") ?? "security/database.json", true, true, "/", true))
+);
 
 export default defaultImpl;
 export type DatabaseAdapter = DatabaseAdapterInterface;
